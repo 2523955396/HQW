42ae198bb201c06c1e7335385036a170837929a6
刘震 <2532955396@qq.com>
2022-03-11 22:01:33 +0800

    1.0

---
+++ app/src/main/java/com/huanqi/hqw/activity/HqwActivity.java
@@ -0,0 +1,29 @@
+package com.huanqi.hqw.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HqwActivity extends AppCompatActivity {
+    Toast toast;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void setToast(String text){
+        if (toast!=null){
+            toast.cancel();
+        }
+        toast= Toast.makeText(this,text,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void GotoActivity(Class<?> activity){
+        startActivity(new Intent(this,activity));
+        finish();
+    }
+}
