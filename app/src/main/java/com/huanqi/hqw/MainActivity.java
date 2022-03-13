3431345dace186cc062f85d81288f7be0458b23c
刘震 <2532955396@qq.com>
2022-03-13 19:59:11 +0800

    测试版v0.3.5

--- app/src/main/java/com/huanqi/hqw/MainActivity.java
+++ app/src/main/java/com/huanqi/hqw/MainActivity.java
@@ -9,10 +9,17 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.huanqi.hqw.Interface.File_download;
 import com.huanqi.hqw.Interface.permission;
 import com.huanqi.hqw.activity.HQWActivity;
+import com.huanqi.hqw.bean.DownloadFileBean;
+import com.huanqi.hqw.file.HQWFile;
+import com.huanqi.hqw.http.HQWHttp;
+
+import java.io.File;
 
 public class MainActivity extends HQWActivity {
+    String url="https://alpan.51huanqi.cn/d/影视站点/电影/电影1/蜘蛛侠英雄无归预告1.mp4";
    String[] PERMISSIONS=new String[]{
            Manifest.permission.WRITE_EXTERNAL_STORAGE,
            Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -25,6 +32,27 @@ public class MainActivity extends HQWActivity {
         HQWpermissions(PERMISSIONS,new permission() {
             @Override
             public void onsucceed() {
+                HQWHttp.DownloadFile(url, HQWFile.SDFile("/视频", "失控玩家"), new File_download() {
+                    @Override
+                    public void progress(long ProgressSize, long MaxSize, String HQWProgressSize, String HQWMaxSize) {
+
+                    }
+
+                    @Override
+                    public void onsuccess(File file, String state) {
+                        setToast(state);
+                    }
+
+                    @Override
+                    public void onfailed() {
+
+                    }
+
+                    @Override
+                    public void console(DownloadFileBean downloadFileBean) {
+
+                    }
+                });
             }
             @Override
             public void onfirstfailure() {
--- app/src/main/java/com/huanqi/hqw/http/HQWHttp.java
+++ app/src/main/java/com/huanqi/hqw/http/HQWHttp.java
@@ -9,7 +9,9 @@ import com.huanqi.hqw.bean.DownloadFileBean;
 import com.huanqi.hqw.file.HQWFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
@@ -24,13 +26,16 @@ import okio.BufferedSink;
 import okio.Okio;
 
 public class HQWHttp {
-    public static void DownloadFile(String url, File file, File_download file_download){
-        OkHttpClient okHttpClient=new OkHttpClient();
+    public static void DownloadFile(String url, File file, File_download file_download) {
+        OkHttpClient okHttpClient = new OkHttpClient();
 //        okHttpClient.newBuilder().readTimeout(1, TimeUnit.MINUTES);
 //        okHttpClient.newBuilder().writeTimeout(1, TimeUnit.MINUTES);
 //        okHttpClient.newBuilder().connectTimeout(5, TimeUnit.MINUTES);
-        Request request=new Request.Builder().url(url).build();
-        Call call=okHttpClient.newCall(request);
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Accept-Encoding", "identity")
+                .build();
+        Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
@@ -39,32 +44,61 @@ public class HQWHttp {
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                    ResponseBody responseBody=response.body();
-                    if (file.length()>=responseBody.contentLength()){
-                        file_download.onsuccess(file,"已下载");
-                    }else {
-                        BufferedSink sink = Okio.buffer(Okio.sink(file));
-                        Timer timer=new Timer();
-                        DownloadFileBean downloadFileBean=new DownloadFileBean();
-                        downloadFileBean.setCall(call);
-                        downloadFileBean.setSink(sink);
-                        downloadFileBean.setTimer(timer);
-                        downloadFileBean.setResponse(response);
-                        timer.schedule(new TimerTask() {
-                            @Override
-                            public void run() {
-                                file_download.progress(file.length(),responseBody.contentLength(),HQWFile.getPrintSize(file.length()),HQWFile.getPrintSize(responseBody.contentLength()));
-                                file_download.onsuccess(file,"下载中");
-                                if (file.length()>=responseBody.contentLength()){
-                                    downloadFileBean.cancel();
-                                    file_download.onsuccess(file,"下载完成");
-                                }
-                            }
-                        },0,1000);
-                        file_download.console(downloadFileBean);
-                        sink.writeAll(responseBody.source());
-                    }
+                ResponseBody responseBody = response.body();
+                BufferedSink sink = Okio.buffer(Okio.sink(file));
+                Timer timer = new Timer();
+                DownloadFileBean downloadFileBean = new DownloadFileBean();
+                downloadFileBean.setCall(call);
+                downloadFileBean.setSink(sink);
+                downloadFileBean.setTimer(timer);
+                downloadFileBean.setResponse(response);
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        file_download.progress(file.length(), responseBody.contentLength(), HQWFile.getPrintSize(file.length()), HQWFile.getPrintSize(responseBody.contentLength()));
+                        file_download.onsuccess(file, "下载中");
 
+                    }
+                }, 0, 1000);
+                file_download.console(downloadFileBean);
+                sink.writeAll(responseBody.source());
+                //下载完成执行
+                downloadFileBean.cancel();
+                file_download.onsuccess(file, "下载完成");
+            }
+        });
+    }
+
+    public static void HttpDownloadFile(Call call, String url, File file, File_download file_download) {
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                file_download.onfailed();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                ResponseBody responseBody = response.body();
+                BufferedSink sink = Okio.buffer(Okio.sink(file));
+                Timer timer = new Timer();
+                DownloadFileBean downloadFileBean = new DownloadFileBean();
+                downloadFileBean.setCall(call);
+                downloadFileBean.setSink(sink);
+                downloadFileBean.setTimer(timer);
+                downloadFileBean.setResponse(response);
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        file_download.progress(file.length(), responseBody.contentLength(), HQWFile.getPrintSize(file.length()), HQWFile.getPrintSize(responseBody.contentLength()));
+                        file_download.onsuccess(file, "下载中");
+
+                    }
+                }, 0, 1000);
+                file_download.console(downloadFileBean);
+                sink.writeAll(responseBody.source());
+                //下载完成执行
+                downloadFileBean.cancel();
+                file_download.onsuccess(file, "下载完成");
             }
         });
     }
