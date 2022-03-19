3120724154f58171e4469b8de9bf8c900be3fd8a
刘震 <2532955396@qq.com>
2022-03-20 01:16:07 +0800

    测试版v0.4.5

--- app/src/main/java/com/huanqi/hqw/MainActivity.java
+++ app/src/main/java/com/huanqi/hqw/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends HQWActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        HQWpermissions(PERMISSIONS,new permission() {
+        HQWPermissions(PERMISSIONS,new permission() {
             @Override
             public void onsucceed() {
 
