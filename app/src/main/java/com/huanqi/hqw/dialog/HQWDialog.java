40fe8fa61d10a4f8489bfe94a81880c7c6413424
刘震 <2532955396@qq.com>
2022-06-20 15:06:12 +0800

    测试版v0.6.3

--- app/src/main/java/com/huanqi/hqw/dialog/HQWDialog.java
+++ app/src/main/java/com/huanqi/hqw/dialog/HQWDialog.java
@@ -2,6 +2,8 @@ package com.huanqi.hqw.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -12,17 +14,11 @@ import com.huanqi.hqw.R;
 
 public class HQWDialog extends Dialog {
 
-
-    public HQWDialog(@NonNull Context context) {
+    public HQWDialog(@NonNull Context context,int layout) {
         super(context);
-    }
-
-    /**
-     * 去除背景阴影
-     */
-    public void removeshadow(){
+        View view = LayoutInflater.from(context).inflate(layout, null);
+        setContentView(view);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        getWindow().setBackgroundDrawableResource(R.color.transparency);
     }
-
-
 }
