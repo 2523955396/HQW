819fe5525c383ab68732f34e3675750562629fc2
刘震 <2532955396@qq.com>
2022-06-20 13:35:55 +0800

    测试版v0.6.0

--- app/src/main/java/com/huanqi/hqw/Utils/HQWJsonUtil.java
+++ app/src/main/java/com/huanqi/hqw/Utils/HQWJsonUtil.java
@@ -1,13 +1,23 @@
 package com.huanqi.hqw.Utils;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class HQWJsonUtil {
 
-    public static String JsontoString(String jsontext,String key){
+    public static String ObjectGetValue(String jsontext,String key){
         String textout="";
         try {
             textout=new JSONObject(jsontext).getString(key);
+        }catch (Exception v){
+        }
+        return textout;
+    }
+
+    public static String ArrayGetValue(String jsontext,int key){
+        String textout="";
+        try {
+            textout=new JSONArray(jsontext).getString(key);
         }catch (Exception v){
         }
         return textout;
