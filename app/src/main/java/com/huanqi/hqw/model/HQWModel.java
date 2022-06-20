248d9fed35cbc5036a6b0418d68fdadab0c31fcc
刘震 <2532955396@qq.com>
2022-06-20 13:42:09 +0800

    测试版v0.6.1

--- app/src/main/java/com/huanqi/hqw/model/HQWModel.java
+++ app/src/main/java/com/huanqi/hqw/model/HQWModel.java
@@ -6,21 +6,31 @@ import android.view.View;
 import com.huanqi.hqw.activity.HQWActivity;
 
 public class HQWModel {
-    HQWActivity activity;
+    HQWActivity hqwActivity;
     View view;
     Context context;
-    public HQWActivity getActivity() {
-        return activity;
+    public HQWActivity getHqwActivity() {
+        return hqwActivity;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void setHqwActivity(HQWActivity hqwActivity) {
+        this.hqwActivity = hqwActivity;
+    }
+
+    public void setView(View view) {
+        this.view = view;
     }
 
     public View getView() {
         return view;
     }
-
     public Context getContext() {
         return context;
     }
-
     public void onCreate() {
     }
     public void onCreate(View view) {
@@ -30,8 +40,8 @@ public class HQWModel {
         this.view=view;
         this.context=context;
     }
-    public void onCreate(HQWActivity activity) {
-        this.activity=activity;
+    public void onCreate(HQWActivity hqwActivity) {
+        this.hqwActivity=hqwActivity;
     }
     public void onPause() {
     }
