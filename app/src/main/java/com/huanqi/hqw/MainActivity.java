accf8a04bc712daeefb1ab3ab5e217aa948ee453
刘震 <2532955396@qq.com>
2022-06-26 20:00:52 +0800

    测试版v0.7.4

--- app/src/main/java/com/huanqi/hqw/MainActivity.java
+++ app/src/main/java/com/huanqi/hqw/MainActivity.java
@@ -28,14 +28,13 @@ import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends HQWActivity implements View.OnClickListener {
+public class MainActivity extends HQWActivity{
     String[] PERMISSIONS = new String[]{
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.ACCESS_COARSE_LOCATION
     };
-    ProgressBar progress;
-    HQWHttp hqwHttp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,13 +42,6 @@ public class MainActivity extends HQWActivity implements View.OnClickListener {
         HQWPermissions(PERMISSIONS, new permission() {
             @Override
             public void onsucceed() {
-
-                hqwHttp=  new HQWHttp();
-                Button xiazai=findViewById(R.id.xiazai);
-                Button zanting=findViewById(R.id.zanting);
-                progress=findViewById(R.id.progress);
-                xiazai.setOnClickListener(MainActivity.this);
-                zanting.setOnClickListener(MainActivity.this);
             }
 
             @Override
@@ -58,43 +50,5 @@ public class MainActivity extends HQWActivity implements View.OnClickListener {
             }
         });
     }
-    String url = "https://alpan.51huanqi.cn/d/影视站点/动漫/动漫2/龙珠超/1.mkv";
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-           case  R.id.xiazai:
-              if ( hqwHttp.isDownloading()){
-                  setToast("正在下载，请勿重复点击");
-              }else {
-                  hqwHttp.DownloadFileResume(url, HQWFileUtil.SDFile("/对对对", "1.mkv"),1000, new FileDownloadCallBack() {
-                      @Override
-                      public void progress(long ProgressSize, long MaxSize, String HQWProgressSize, String HQWMaxSize) {
-                          progress.setProgress((int)ProgressSize);
-                          progress.setMax((int)MaxSize);
-                      }
 
-                      @Override
-                      public void onsuccess(File file, int state) {
-                          if (state==HQWHttp.DOWNLOADING){
-                              setToast("下载中");
-                          }else if (state==HQWHttp.COMPLETE){
-                              setToast("下载完成");
-                          }
-                      }
-
-                      @Override
-                      public void onfailed() {
-
-                      }
-                  });
-              }
-
-
-            break;
-            case  R.id.zanting:
-                hqwHttp.cancelResume();
-                setToast("下载停止");
-                break;
-        }
-    }
 }
