81a25fa01022cb62474782cb38b77fb201738747
刘震 <2532955396@qq.com>
2022-07-18 14:25:02 +0800

    测试版v0.8.1

---
+++ app/src/main/java/com/huanqi/hqw/Utils/HQWTimeUtil.java
@@ -0,0 +1,28 @@
+package com.huanqi.hqw.Utils;
+
+import java.util.Formatter;
+import java.util.Locale;
+
+/**
+ * 帧时间处理类
+ */
+public class HQWTimeUtil {
+
+    //视频时间转换
+    public static String ValueForTime(int timeMs) {
+        if (timeMs <= 0 || timeMs >= 24 * 60 * 60 * 1000) {
+            return "00:00";
+        }
+        int totalSeconds = timeMs / 1000;
+        int seconds = totalSeconds % 60;
+        int minutes = (totalSeconds / 60) % 60;
+        int hours = totalSeconds / 3600;
+        StringBuilder stringBuilder = new StringBuilder();
+        Formatter mFormatter = new Formatter(stringBuilder, Locale.getDefault());
+        if (hours > 0) {
+            return mFormatter.format("%d:%02d:%02d", hours, minutes, seconds).toString();
+        } else {
+            return mFormatter.format("%02d:%02d", minutes, seconds).toString();
+        }
+    }
+}
