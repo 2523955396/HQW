fd0b54dd24b78daaff42104b3ec296c8fb053ac8
刘震 <2523955396@qq.com>
2022-12-27 17:26:29 +0800

    解决RecyclerView与ScrollView冲突问题

---
+++ app/src/main/java/com/huanqi/hqw/weight/HQWRecyclerView.java
@@ -0,0 +1,28 @@
+package com.huanqi.hqw.weight;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HQWRecyclerView extends RecyclerView {
+    public HQWRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HQWRecyclerView(Context context) {
+        super(context);
+    }
+
+    public HQWRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(
+                Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
+}
