c3e6d2eb46bbd8e8ae886122c56c38eddb3bac37
刘震 <2523955396@qq.com>
2023-08-05 19:06:46 +0800

    适配kotlin

--- app/src/main/java/com/huanqi/hqw/MainActivity.java
+++ app/src/main/java/com/huanqi/hqw/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import com.huanqi.android.Interface.HQWPermission;
 import com.huanqi.android.activity.HQWActivity;
-import com.huanqi.hqw.databinding.ActivityMainBinding;
+
 import com.huanqi.http.upload.HQWUploadManger;
 
 import org.json.JSONArray;
@@ -18,49 +18,14 @@ import java.io.IOException;
 import java.util.List;
 
 
-public class MainActivity extends HQWActivity implements View.OnClickListener {
+public class MainActivity extends HQWActivity {
 
-    String[] Premission = new String[]{
-//            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.READ_MEDIA_IMAGES,
-            Manifest.permission.READ_MEDIA_AUDIO,
-            Manifest.permission.READ_MEDIA_VIDEO,
-            Manifest.permission.QUERY_ALL_PACKAGES
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        HQWPermissions(Premission, new HQWPermission() {
-            @Override
-            public void onsucceed() {
-
-            }
-
-            @Override
-            public void onfailure() {
-                HQWPermissions(Premission, new HQWPermission() {
-                    @Override
-                    public void onsucceed() {
-
-                    }
-
-                    @Override
-                    public void onfailure() {
-
-                    }
-                });
-            }
-        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.pb_ssssss:
-                break;
-        }
-    }
+
 }
